e6b74f4a479801a6b5c2637a67367764c1630cac
Thomas Benedict <tomtombene@gmail.com>
2018-07-07 10:56:58 -0600

    fixed merge

--- server/controllers/createRoom.js
+++ server/controllers/createRoom.js
@@ -2,7 +2,6 @@ const Room = require('../models/room');
 
 exports.createRoom = function (req, res, next) {
 	Room.create({
-<<<<<<< HEAD
 		roomCode: null,
 		activeRoom: true
 	})
@@ -16,16 +15,3 @@ exports.createRoom = function (req, res, next) {
 		.then(dbResponse => res.json(dbResponse))
 		.catch(err => res.status(500).send('create room error: ' + err))
 };
-=======
-			roomCode: null,
-			activeRoom: true
-		})
-		.then(dbResponse => Room.findOneAndUpdate({
-			_id: dbResponse._id
-		}, {
-			roomCode: dbResponse._id.toString().slice(-5)
-		}, (err,doc) => console.log(doc)))
-		.then(dbResponse => res.json(dbResponse))
-		.catch(err => res.status(500).send('create room error: ' + err))
-};
->>>>>>> 430bbd173d4cf9ed7790428e7f662cf493391be5

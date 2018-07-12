3161a5f6212b6efaf94d8ab41c1c5c623db2b151
Thomas Benedict <tomtombene@gmail.com>
2018-07-12 12:32:22 -0600

    not sure what changed sense last pull

--- client/components/GameGrid.js
+++ client/components/GameGrid.js
@@ -36,6 +36,7 @@ class GameGrid extends React.Component {
 			{!this.props.auth.isSignedIn && (
 
 				<GoogleLogin 
+				className="button"
 				clientId="368878887068-dqo0j4ru3m1uk6jgsjuvh823lq0871d2.apps.googleusercontent.com"
 				buttonText="Login"
 				onSuccess={this.onSignIn}

365a7cb91b4ce071065f5583240563368586e334
Amy Harris <akharris35@hotmail.com>
2018-07-12 19:33:36 -0600

    testing server

---
+++ client/components/instructions/instructions.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+// class Instructions extends Component {
+    
+// 		}
+		
+	
+//     render() {
+
+//         return (
+           
+//         );
+//     }
+// }
+
+// function mapStateToProps(state) {
+//     return {
+// 				connection: state.connections
+//     }
+// }
+
+// //Higher order component
+// export default connect(mapStateToProps)(Instructions);
+

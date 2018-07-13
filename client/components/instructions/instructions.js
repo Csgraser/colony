7b36671c908ed758bebc856cb8a582106886b990
Tyler Schmauch <tschmauch@hotmail.com>
2018-07-13 12:22:25 -0600

    changed instructions, started on UserCard, started on GameHostCard, some styling, everything seems to be working that was working last night

--- client/components/instructions/instructions.js
+++ client/components/instructions/instructions.js
@@ -4,7 +4,10 @@ import "./instructions.styl"
 const Instructions = (props) => (
     <div className="instructions">
         <ol className="heading">How to Play</ol>
-        <li className="item">Click on Create Game</li>
+        <li className="item">Click on "Login & Create Game Room"</li>
+				<li className="item">Users Go to Website on Personal Device</li>
+				<li className="item">Click Join a Game and enter Game Code and Name, then press Enter Room  </li>
+				<li className="item">Once all Users have joined Room, click Start Game on host device - and let the app do the rest!</li>
     </div>
 );
 

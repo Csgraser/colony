78d0dc1b65f2f23e82b34cce8e062b559a102831
Amy Harris <akharris35@hotmail.com>
2018-07-12 21:33:54 -0600

    heroku test

--- client/data/config.js
+++ client/data/config.js
@@ -1,10 +1,10 @@
 import {} from 'dotenv/config';
 import Raven from 'raven-js';
-import KEYS from '../../config/keys_dev.js';
+// import KEYS from '../../config/keys_dev.js';
 import Keystest from '../../keys';
 //TODO get keys from .env file
 // console.log(Keystest);
-const sentry_key = Keystest.sentry_key || KEYS.sentry_key;
+const sentry_key = Keystest.sentry_key;
 
 const sentry_app = '1234579';
 export const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;

d7b6075f8e7b959929996380def588ef19eb55d1
Merek Smith <mereksmith@hotmail.com>
2018-07-13 18:18:11 -0600

    socket stuff

--- server/socket_server.js
+++ server/socket_server.js
@@ -20,7 +20,7 @@ module.exports = function (socket) {
 		if (isUser(connectedUsers, nickname)) {
 			callback({ isUser: true, user: null })
 		} else {
-			callback({ isUser: false, user: createUser({ name: nickname }) })
+			callback({ isUser: false, user: createUser({ name: nickname, socketId:socket.id }) })
 		}
 	})
 

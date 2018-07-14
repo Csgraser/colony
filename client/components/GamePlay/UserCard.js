7d4c193c90467907dc308e69baedad62d0380618
Csgraser <csgraser@gmail.com>
2018-07-13 20:03:30 -0600

    working on userlanding, characters.json, and instructions page

--- client/components/GamePlay/UserCard.js
+++ client/components/GamePlay/UserCard.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { colonists, infected } from '../../data/characters';
+import { characters } from '../../data/characters';
 // import playerImage from `${colonists[0].photo}`;
 
 export default function UserCard(props) {
 	return (
 		<div className="UserCard">
 			<div className="UserCard-image" />
-			<span className="UserCard-name">Name: {console.log("usercard props",this)}</span>
+			<span className="UserCard-name">Name: {console.log("usercard props", this)}</span>
 			<span className="UserCard-role">Role: {"this should be the assigned user's role infected or colonist"}</span>
 		</div>
 	)
 }
+
+
+function displayColonistInfo(colonists) {
+	let i = colonists.length - 1;
+	for (; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = colonists[i];
+		colonists[i] = colonists[j];
+		colonists[j] = temp;
+	}
+	return colonists;
+};
+
+class RandomCharacter extends React.Component {
+  render() {
+    const shuffledCharacter = displayColonistInfo(this.props.colonists);
+    return (
+      <ul>
+        {shuffledCharacter.map((colonists, idx) => {
+          return (
+            <li key={idx}>
+              <p>{colonists.name}</p>
+              <image>{colonists.photo}</image>
+              {/* <Link to={`/blog/post-1/:${post.id}`}>Weiter lesen</Link> */}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+RandomCharacter.propTypes = {
+  colonists: React.PropTypes.colonists,
+};
+
+
+// 	var randomColonist = colonists[Math.floor(Math.random() * colonists.length)];
+
+// 	document.UserCard.innerHTML = randomColonist
--- client/components/instructions/instructions.js
+++ client/components/instructions/instructions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./instructions.styl"
+import characters from '../../data/characters';
 
 const Instructions = (props) => (
     <div className="instructions">
@@ -8,8 +9,15 @@ const Instructions = (props) => (
 				<li className="item">Users Go to Website on Personal Device</li>
 				<li className="item">Click Join a Game and enter Game Code and Name, then press Enter Room  </li>
 				<li className="item">Once all Users have joined Room, click Start Game on host device - and let the app do the rest!</li>
+				<br/>
+				{characters.map((photo, index) => {
+			return <div className="photoContainer" key={index} ><img className="photos" src={photo.photo} alt={photo.name}
+			/></div>;
+		})}
     </div>
+
 );
+
 
 export default Instructions
 

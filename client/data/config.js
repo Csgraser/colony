3cd0a7e9a0b339279270c5680fad94a8e9329e67
Merek Smith <mereksmith@hotmail.com>
2018-07-14 09:18:24 -0600

    sentry key

--- client/data/config.js
+++ client/data/config.js
@@ -4,7 +4,7 @@ import Raven from 'raven-js';
 import Keystest from '../../keys';
 //TODO get keys from .env file
 // console.log(Keystest);
-const sentry_key = Keystest.sentry_key;
+const sentry_key = "5e629f0c9e2248e0811e3d6abfd2ca5a";
 
 const sentry_app = '1234579';
 export const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
